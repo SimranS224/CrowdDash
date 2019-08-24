8821dc44fd4060ee70f675222d1660c499888046
Jason Chin <jason.rob.chin@gmail.com>
2019-08-24 00:59:15 -0400

    add migrations and fix run script

--- backend/api/app.py
+++ backend/api/app.py
@@ -8,5 +8,6 @@ from .settings import BASE_DIR
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
